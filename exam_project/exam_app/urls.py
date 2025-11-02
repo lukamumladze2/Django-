b44f4bb96eeae9dd1d5842511a5a34e4d4dea3ff
lukamumladze2 <mumladzel258@gmail.com>
2025-11-02 12:34:42 +0100

    Django – საკონტროლო

--- exam_project/exam_app/urls.py
+++ exam_project/exam_app/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('films/', views.films, name='films'),
+    path('admin/', admin.site.urls),
+    path('', include('exam_app.urls')),
 ]
